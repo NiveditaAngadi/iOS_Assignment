e681759d22a374843f11db1d8b0ada5dc36544d0
Nivedita Angadi <nivedita@Niveditas-MacBook-Pro.local>
2024-07-20 16:24:55 +0530

    SwiftUI version of the app

---
+++ ios-case-study-SwiftUI/ProductViewer/ProductViewer/ProductViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductViewerApp.swift
+//  ProductViewer
+//
+//  Created by Nivedita Angadi on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
